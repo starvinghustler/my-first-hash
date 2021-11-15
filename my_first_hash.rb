6b317e24f08d8fa1d227d96863820dc020c86ecf
idobok <idobok12@gmail.com>
2021-11-15 14:39:28 +0200

    commit tree 1fa2a786e4c2526de267ee30679c3154ea82f0e5

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+tasks_to_do = {
+  "collect photos" => 4000,
+  "build deck" => "urgent",
+  "find employees" => "operations"
+}
   
 end
 
@@ -9,7 +13,11 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+shipping_manifest = {
+  "whale bone corsets" => 5,
+  "porcelain vases" => 2,
+  "oil paintings" => 3
+}
 
 end
 
@@ -21,6 +29,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -31,7 +40,8 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
 
   # add 4 gun powder to the shipping_manifest hash below
@@ -39,5 +49,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
